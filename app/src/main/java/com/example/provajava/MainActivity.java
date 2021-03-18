8e8541a027d32e214644df7f10f55e53ffae71c3
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2021-03-18 23:53:29 +0100

    changed email verification icon, text centered after changing copy

--- app/src/main/java/com/example/provajava/MainActivity.java
+++ app/src/main/java/com/example/provajava/MainActivity.java
@@ -6,17 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text, logout, email, phone;
+    TextView userText, logout, email, phone;
+    ImageView verification;
+    boolean emailVerified;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+    FirebaseUser user;
     String userID;
 
     @Override
@@ -25,22 +30,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // inputs
-        text = findViewById(R.id.main);
+        userText = findViewById(R.id.main);
         logout = findViewById(R.id.logout);
         email = findViewById(R.id.profileEmail);
         phone = findViewById(R.id.profilePhone);
+        verification = findViewById(R.id.verified);
 
         // firebase
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        userID = mAuth.getCurrentUser().getUid();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userID = user.getUid();
+        emailVerified = user.isEmailVerified();
 
         db.collection("users").document(userID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
+                // sets user data in profile
                 email.setText(documentSnapshot.getString("email"));
                 phone.setText(documentSnapshot.getString("phone"));
-                text.setText("Hello " + documentSnapshot.getString("user") + ", this is your profile!");
+                userText.setText("Hello " + documentSnapshot.getString("user") + ",\nthis is your profile!");
+                userText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER); // recenter the text
+
+                // check for mail verification
+                if (emailVerified){
+                    verification.setVisibility(View.VISIBLE);
+                } else {
+                    verification.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
